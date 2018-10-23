3ff8abb887bda4846c099e73caf11a9184a7076a
DarioLinton3629 <dari3629@gmail.com>
2018-10-23 16:03:33 -0500

    exercise 3

--- maze.js
+++ maze.js
@@ -1,16 +1,36 @@
+var lose = false ;
+
 window.onload = function(){
   myFunction()
 };
+
  function myFunction() {
   var x = document.querySelectorAll("#maze div.boundary");
+  var end = document.getElementById("end");
+
   for (var i = 0; i < x.length; i++) {
     x[i].addEventListener("mouseover", colour);
   }
+  
+  end.addEventListener("mouseover", end1);
+  
 }
+
+function end1(){
+  if (lose){
+    alert("you lose")
+  } else {
+    alert("you win");
+  }
+	
+}
+
  function colour() {
+  lose = true;
   var x = document.querySelectorAll("#maze div.boundary");
   for (var i = 0; i < x.length; i++) {
     
     x[i].classList.add("youlose");
   }
 } 
+
